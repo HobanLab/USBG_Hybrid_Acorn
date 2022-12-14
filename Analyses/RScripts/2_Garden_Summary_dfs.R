f84d4b6affa0672dc066691590cb1b3e110257de
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2022-12-14 09:59:42 -0600

    adding new script to process all accessioned trees in TMA to only limit to quercus sp

--- Analyses/RScripts/2_Garden_Summary_dfs.R
+++ Analyses/RScripts/2_Garden_Summary_dfs.R
@@ -22,14 +22,15 @@ for(garden in 1:length(garden_list_occ)){
   
   species_list <- unique(sp_df$Species_Name)
   
+  garden_species_df <- matrix(nrow = length(species_list), ncol = 1)
+  
+  rownames(garden_species_df) <- species_list
+  
   for(sp in 1:length(species_list)){
     
-    garden_species_df <- matrix(nrow = length(species_list), ncol = 1)
-    
     garden_species_df[sp,1] <- length(sp_df[sp_df$Species_Name == species_list[[sp]],][,1])
-    rownames(garden_species_df) <- species_list
-    
-    write.csv(garden_species_df, paste0(gsub("\\_.*",'', garden_list_occ[[garden]]), "_species_df.csv"))
+   
+    write.csv(garden_species_df, paste0(gsub("\\..*",'', garden_list_occ[[garden]]), "_species_df.csv"))
     
   }
   
---
+++ Analyses/RScripts/3_TMA_all_trees.R
@@ -0,0 +1,19 @@
+#This script was created to clean the occ records of all accessioned trees
+#at the arb and limit to only the oak trees
+
+#####################
+#     Libraries     #
+#####################
+
+library(dplyr)
+
+#####################
+#     Analysis      #
+#####################
+
+#set wd 
+setwd("")
+
+TMA_all_trees <- 
+
+
